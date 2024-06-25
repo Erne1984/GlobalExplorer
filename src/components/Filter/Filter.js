42d1e2b684f5b5552698dd6fe38734020f43ed2e
Erne1984 <ernestofamorim@gmail.com>
2024-06-25 16:36:08 -0300

    working on filter

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -2,11 +2,28 @@ import './Filter.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCaretDown } from '@fortawesome/free-solid-svg-icons';
 
-export default function Filter(){
-    return(
-        <div className='filter-box'>
-            <span className='filter-text'>Filter by region</span>
-            <FontAwesomeIcon icon={faCaretDown} />
-        </div>
+export default function Filter() {
+    return (
+        <>
+            <div className='filter-container'>
+
+                <div className='filter-box'>
+                    <span className='filter-text'>Filter by region</span>
+                    <FontAwesomeIcon icon={faCaretDown} />
+                </div>
+
+                <div className='dropdown-menu'>
+
+                    <ul>
+                        <li>Africa</li>
+                        <li>America</li>
+                        <li>Europe</li>
+                        <li>Asia</li>
+                        <li>Oceania</li>
+                    </ul>
+
+                </div>
+            </div>
+        </>
     )
 }
