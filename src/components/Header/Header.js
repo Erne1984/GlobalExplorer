25ded96548ffca98344aaa2545465ed665002528
Erne1984 <ernestofamorim@gmail.com>
2024-06-27 09:11:10 -0300

    favicon and responsivity

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,39 +5,23 @@ import { faMoon, faSun } from '@fortawesome/free-solid-svg-icons';
 export default function Header({ toggleDarkMode, isDarkMode }) {
 
     return (
-        <>
-            <header className='main-header-container'>
-
-                <div className="main-header">
-
-                    <h1 className='title'>Where is in the world?</h1>
-
-                    <div className='switch-theme-box' onClick={toggleDarkMode}>
-
-                        {
-                            !isDarkMode ?
-                                (
-                                    <>
-                                        <FontAwesomeIcon icon={faSun} className='icons' />
-
-                                        <div className='theme-text'>White Mode</div>
-                                    </>
-                                )
-                                :
-                                (
-                                    <>
-                                        <FontAwesomeIcon icon={faMoon} className='icons' />
-
-                                        <div className='theme-text'>Dark Mode</div>
-                                    </>
-                                )
-                        }
-
-                    </div>
-
+        <header className='main-header-container'>
+            <div className="main-header">
+                <h1 className='title'>Where in the world?</h1>
+                <div className='switch-theme-box' onClick={toggleDarkMode}>
+                    {!isDarkMode ? (
+                        <>
+                            <FontAwesomeIcon icon={faSun} className='icons' />
+                            <div className='theme-text'>White Mode</div>
+                        </>
+                    ) : (
+                        <>
+                            <FontAwesomeIcon icon={faMoon} className='icons' />
+                            <div className='theme-text'>Dark Mode</div>
+                        </>
+                    )}
                 </div>
-
-            </header>
-        </>
+            </div>
+        </header>
     )
 }
